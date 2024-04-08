15e635cbd1b16757c1b20b68561307e2f6fdc9b2
soare <soare@DESKTOP-62377LU>
2024-04-08 00:53:47 -0300

    Adicionar arquivos de projeto.

---
+++ PDI/Controllers/BotController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PDI.Models;
+
+namespace PDI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BotController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/copyAttendance")]
+        public IActionResult CopyAttendance([FromBody] CopyConfigurationRequest copyConfigurationRequest)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ PDI/Models/CopyConfigurationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace PDI.Models
+{
+    public class CopyConfigurationRequest
+    {
+        public required string OriginTenant { get; set; }
+        public required string OriginAuthorization { get; set; }
+        public required string TargetTenant { get; set; }
+        public required string TargetAuthorization { get; set; }
+    }
+}
